44cb2bf611c1e2f1373cb3fd9b69f4935d164299
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-12-27 23:26:34 +0530

    fix of post.fullDetail instead the description in postDetailPage and also removed the discription from ALlposts cards from top

--- backend/services/fetchPostsService.ts
+++ backend/services/fetchPostsService.ts
@@ -1,12 +1,13 @@
 import { db } from "../drizzle/db.js"
 import { postsTable } from "../drizzle/tables.js"
+import { desc } from "drizzle-orm"
 
 
 // db function 
 export const fetchPostsFunc = async () => {
 
     try {
-        const data = await db.select().from(postsTable);
+        const data = await db.select().from(postsTable).orderBy(desc(postsTable.updated_at));
         return data;
     } catch (err) {
         console.error(err);
