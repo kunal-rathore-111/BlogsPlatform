7c2d4a21380755341c427a15128977606823fac6
kunal-hp <kunalworkspace111@gmail.com>
2025-12-28 22:47:35 +0530

    Fix ESM import: add .js extension to updatePostService

--- backend/controller/updatePostController.ts
+++ backend/controller/updatePostController.ts
@@ -1,5 +1,5 @@
 import type { RequestHandler } from "express";
-import { updatePostFunc } from "../services/updatePostService";
+import { updatePostFunc } from "../services/updatePostService.js";
 
 
 
