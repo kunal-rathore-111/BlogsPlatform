4bff16f20e683c491478681bd366c0c2d704c464
kunal-hp <kunalworkspace111@gmail.com>
2025-12-28 22:55:59 +0530

    fixed the loading nocontent bug by putting the loader on the top of the entire dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ import { PostDetailPage } from "./Pages/PostDetailPage";
 import { AllPostsPage } from "./Pages/AllPostsPage";
 import { PostsDataContextProvider } from "./contextProvider/postsDataContext";
 import { useFetch } from "./hooks/queryHooks/useFetch";
+import { useSelector } from "react-redux";
+import type { RootState } from "./store/store";
 
 
 
@@ -55,6 +57,16 @@ function LenisWrapper({ children }: { children: React.ReactNode }) {
 export default function App() {
   const { data, isLoading, isError, error } = useFetch(); // runs only ones when the HomePage mounts
 
+  const isDark = useSelector((state: RootState) => state.theme.isDark)
+
+  if (isLoading) return <div>
+    <div className={`min-h-screen flex items-center justify-center ${isDark ? 'bg-black' : 'bg-white'}`}>
+      <svg className="animate-spin h-12 w-12 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" role="img" aria-label="Loading">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+      </svg>
+    </div>
+  </div>
 
   return (
     <PostsDataContextProvider props={{ data, isLoading, isError, error }}>
--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -36,17 +36,9 @@ export function HomePage() {
         // TODO: render a fallback (spinner/skeleton) or return null
         return null;
     }
-    const { isLoading, error, isError, data } = postData;
+    const { error, isError, data } = postData;
 
 
-    if (isLoading) return <div>
-        <div className={`min-h-screen flex items-center justify-center ${isDark ? 'bg-black' : 'bg-white'}`}>
-            <svg className="animate-spin h-12 w-12 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" role="img" aria-label="Loading">
-                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
-            </svg>
-        </div>
-    </div>
 
     if (isError) return (
         <div className={`min-h-screen flex items-center justify-center ${isDark ? 'bg-black' : 'bg-white'}`}>
